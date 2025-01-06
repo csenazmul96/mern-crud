b4badc802fc43cbecddd43c4c3db015ce329037d
csenazmul96 <csenazmul96@gmail.com>
2025-01-06 12:52:31 +0600

    dark and light mode theme

--- front-end/src/components/product/ProductForm.jsx
+++ front-end/src/components/product/ProductForm.jsx
@@ -1,19 +1,43 @@
 import {Button, Input, Stack} from "@chakra-ui/react";
 import { Field } from "@/components/ui/field"
+import {useState} from "react";
 
 const ProductForm = () => {
+    const [form, setForm] = useState({
+        name: "",
+        price: "",
+        image: ""
+    })
+    const handleClick = () => {
+      console.log(form)
+    }
     return (
         <Stack gap="5" maxW="full" css={{ "--field-label-width": "96px" }} >
             <Field label="Product Name" invalid errorText="This is an error text">
-                <Input placeholder="Name" />
+                <Input
+                    placeholder="Name"
+                    name="name"
+                    value={form.name}
+                    onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+                />
             </Field>
             <Field label="Price">
-                <Input placeholder="Price" type="number"  />
+                <Input
+                    placeholder="Price"
+                    type="number"
+                    name="price"
+                    value={form.price}
+                    onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+                />
             </Field>
             <Field label="Image">
-                <Input placeholder="Image" type="file"  />
+                <Input
+                    placeholder="Image"
+                    name="image"
+                    onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+                />
             </Field>
-            <Button>Click me</Button>
+            <Button onClick={handleClick}>Click me</Button>
         </Stack>
     )
 }
